c59555e82b81e3de0663c5c1bc04b168e0ab78ee
J_Wyndham <joseph.wyndham@uts.edu.au>
2017-08-08 21:19:08 +1000

    built cost curve and lowest cost envelope calculators

--- core.py
+++ core.py
@@ -1,4 +1,4 @@
-from transformations import generate_load_duration_curve
+from transformations import generate_load_duration_curve, find_lowest_cost_envelope
 import pandas as pd
 import os
 import matplotlib.pyplot as plt
@@ -57,30 +57,38 @@ gas_price_per_mwh = 3.6 * user_inputs_dict['Value']['Gas price ($/GJ)']
 coal_price_per_mwh = 3.6 * user_inputs_dict['Value']['Coal price ($/GJ)']
 biomass_fuel_price_per_mwh = user_inputs_dict['Value']['Biomass Fuel Price ($/MWh)']
 # assign generator fuel cost per MWh based on user input (non-matching fuel types will be returned as $0/MWh)
-generators_included_characteristics_dataframe['gas_price_cost'] = np.where(generators_included_characteristics_dataframe['Fuel Type'] == 'Gas', gas_price_per_mwh, 0)
-generators_included_characteristics_dataframe['coal_price_cost'] = np.where(generators_included_characteristics_dataframe['Fuel Type'] == 'Coal', coal_price_per_mwh, 0)
-generators_included_characteristics_dataframe['biomass_price_cost'] = np.where(generators_included_characteristics_dataframe['Fuel Type'] == 'Biomass', biomass_fuel_price_per_mwh, 0)
+generators_included_characteristics_dataframe['gas_price_cost'] = np.where(
+    generators_included_characteristics_dataframe['Fuel Type'] == 'Gas', gas_price_per_mwh, 0)
+generators_included_characteristics_dataframe['coal_price_cost'] = np.where(
+    generators_included_characteristics_dataframe['Fuel Type'] == 'Coal', coal_price_per_mwh, 0)
+generators_included_characteristics_dataframe['biomass_price_cost'] = np.where(
+    generators_included_characteristics_dataframe['Fuel Type'] == 'Biomass', biomass_fuel_price_per_mwh, 0)
 # calculate total fuel cost ($/MW/yr) accounting for thermal efficiency of plant
-generators_included_characteristics_dataframe['total_fuel_cost'] = 365 * 24 * (generators_included_characteristics_dataframe['gas_price_cost'] + generators_included_characteristics_dataframe['coal_price_cost'] + generators_included_characteristics_dataframe['biomass_price_cost']) / generators_included_characteristics_dataframe['Thermal Efficiency']
+generators_included_characteristics_dataframe['total_fuel_cost'] = (365 * 24 * (generators_included_characteristics_dataframe['gas_price_cost']
+    + generators_included_characteristics_dataframe['coal_price_cost']
+    + generators_included_characteristics_dataframe['biomass_price_cost'])
+    /generators_included_characteristics_dataframe['Thermal Efficiency'])
 # calculate total emmissions cost (KgCO2e/MW/yr)
-generators_included_characteristics_dataframe['total_emissions_cost'] = 24 * 365 * generators_included_characteristics_dataframe['Carbon Emissions (KgCO2e/MWh)'] * carbon_price_per_kg
+generators_included_characteristics_dataframe['total_emissions_cost'] = (
+    24 * 365 * generators_included_characteristics_dataframe['Carbon Emissions (KgCO2e/MWh)']
+    * carbon_price_per_kg)
 
 
 # calculate total variable cost
-generators_included_characteristics_dataframe['total_variable_cost'] = generators_included_characteristics_dataframe['VOM/year ($/MW/yr)'] + generators_included_characteristics_dataframe['total_emissions_cost'] + generators_included_characteristics_dataframe['total_fuel_cost']
+generators_included_characteristics_dataframe['total_variable_cost'] = (generators_included_characteristics_dataframe['VOM/year ($/MW/yr)']
+    + generators_included_characteristics_dataframe['total_emissions_cost']
+    + generators_included_characteristics_dataframe['total_fuel_cost'])
 # calculate total fixed cost
-generators_included_characteristics_dataframe['total_fixed_cost'] = generators_included_characteristics_dataframe['Annualised Capital ($/MW/yr)'] + generators_included_characteristics_dataframe['FOM ($/MW/yr)']
+generators_included_characteristics_dataframe['total_fixed_cost'] = (generators_included_characteristics_dataframe['Annualised Capital ($/MW/yr)']
+    + generators_included_characteristics_dataframe['FOM ($/MW/yr)'])
 
 generator_cost_curve_dict = dict()
 for generator in generators_included_list:
-    generator_cost_curve_dict[generator] = {'gradient': generators_included_characteristics_dataframe.loc[generator, 'total_variable_cost'], 'y-intercept': generators_included_characteristics_dataframe.loc[generator, 'total_fixed_cost']}
-
-print generator_cost_curve_dict
+    generator_cost_curve_dict[generator] = {'gradient': generators_included_characteristics_dataframe.loc[generator, 'total_variable_cost'], 'y_intercept': generators_included_characteristics_dataframe.loc[generator, 'total_fixed_cost']}
 
 # caclulate lowest envelope
-# find_lowest_cost_envelope(generator_cost_curve_dict)
-
-
+lowest_cost_envelope_dict = find_lowest_cost_envelope(generator_cost_curve_dict)
+print lowest_cost_envelope_dict
 ### LDC and capacity
 # intersection of CF and LDC
 # calculation of areas under LDC

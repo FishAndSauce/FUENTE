b0cc7ab65202ee481e535ffeb1c4a55b0032dbb4
J_Wyndham <joseph.wyndham@uts.edu.au>
2017-08-24 18:13:18 +1000

    fixed up merge issues in core

--- core.py
+++ core.py
@@ -9,14 +9,6 @@ working_data_store = pd.HDFStore('working_data_store.h5')
 
 hourly_demand_dataframe = working_data_store['hourly_demand_dataframe']
 
-demand_profile = PowerDemandTimeSeries(
-    hourly_demand_dataframe['hourly_demand'],
-    power_unit="MW",
-    time_unit="hours",
-    time_interval=1
-)
-load_duration_curve = demand_profile.create_load_duration_curve(as_percent=False, as_proportion=True, granularity=1000)
-
 generators_included_characteristics_dataframe = working_data_store['generators_included_characteristics_dataframe']
 hourly_demand_dataframe = working_data_store['hourly_demand_dataframe']
 
@@ -41,7 +33,7 @@ start = time.clock()
 demand_profile = PowerDemandTimeSeries(
     hourly_demand_dataframe['hourly_demand'],
     power_unit="MW",
-    time_unit="h",
+    time_unit="hours",
     time_interval=1
 )
 print 'PowerDemandTimeSeries ', time.clock() - start
